14b98cf27356de168b9af5919e37c98912a16fea
Arex360 <49303647+Arex360@users.noreply.github.com>
2021-07-14 10:04:29 +0500

    Update colorDetector.js

--- colorDetector.js
+++ colorDetector.js
@@ -1,20 +1,17 @@
-var synaptic = require('synaptic'); 
-var Neuron = synaptic.Neuron,
-	Layer = synaptic.Layer,
-	Network = synaptic.Network,
-	Trainer = synaptic.Trainer,
-    Architect = synaptic.Architect;
-var inputLayer = new Layer(3);
-var hiddenLayer = new Layer(6);
-var outputLayer = new Layer(3);
+const synaptic = require('synaptic'); 
+const Layer = synaptic.Layer
+const Network = synaptic.Network
+let inputLayer = new Layer(3);
+let hiddenLayer = new Layer(9);
+let outputLayer = new Layer(3);
     inputLayer.project(hiddenLayer);
     hiddenLayer.project(outputLayer);
-var network = new Network({
+let network = new Network({
         input: inputLayer,
         hidden: [hiddenLayer],
         output: outputLayer
     });
-var learningRate = .3;
+let learningRate = .01;
 for (var i = 0; i < 20000; i++){
         network.activate([255,0,0]);
         network.propagate(learningRate, [1,0,0]);
@@ -22,11 +19,27 @@ for (var i = 0; i < 20000; i++){
         network.propagate(learningRate, [0,1,0]);
         network.activate([0,0,255]);
         network.propagate(learningRate, [0,0,1]);
-        network.activate([255,0,0]);
+        network.activate([10,1,5]);
         network.propagate(learningRate, [1,0,0]);
-        network.activate([0,255,0]);
+        network.activate([100,200,10]);
         network.propagate(learningRate, [0,1,0]);
-        network.activate([0,0,255]);
+        network.activate([10,50,255]);
         network.propagate(learningRate, [0,0,1]);
     }
-    console.log(network.activate([255,0,0])); 
+let result = network.activate([255,200,100])
+
+
+if(result[0] > result[1] && result[0] > result[2]){
+    console.log("The color seems to be red")
+}else if(result[1] > result[0] && result[1] > result[2]){
+    console.log("The color seems to be green")
+}else{
+    console.log("The color seems to be blue")
+}
+result = result.map(out=>{
+    out = out * 100
+    out = out + ''
+    out = out.substring(0,5)
+    return out + "%"
+})
+console.log(result); 
